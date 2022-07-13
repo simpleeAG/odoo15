c126750b8ec85c1791d2821f6080812edd3d5f86
Pinal Timbadiya <pinalt1998>
2022-07-13 13:31:49 +0530

    Add quotation template

---
+++ quotation_report_customization_pt/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Simplee Sale Report",
+    'author': 'Pinal Timbadiya',
+    'category': 'sale',
+    'summary': """Simplee Sale Report customizaion""",
+    'maintainer': 'Pinal Timbadiya',
+    'description': """Simplee Sale Report customizaion""",
+    'version': '1.0',
+    'depends': ['sale_management'],
+    'data': [
+              'report/new_sale_report_view.xml',
+            ],
+    'license': 'OPL-1',
+    'price': 170.00,
+    'currency': 'USD',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
